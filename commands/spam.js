224d9123732d815546529df9498b6f6f4d6d7453
HannahGuo <hguo2102@gmail.com>
2020-08-04 17:35:30 -0400

    spam command woot

---
+++ commands/spam.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: 'spam',
+	description: 'Spam ping your friends, because who wouldn\'t??',
+	guildOnly: true,
+	usage: '<user>',
+	execute(message, args) {
+		if (args.length != 1 || !args[0].match(/<@.?[0-9]*?>/)) {
+			message.channel.send("Please only use a tagged user as an argument! Example: !ping @Person");
+			return;
+		} else if ((args[0].includes("<@317326420617592833>") || args[0].includes("<@317326420617592833>")) & message.author.username != "Myself⁷") {
+			message.channel.send("No you can't spam ping the creator. That's mean :( ");
+			return;
+		}
+
+		let messageToSend = "";
+		let seperator = "\n";
+		while (messageToSend.length < 2000 - args[0].length - seperator.length) {
+			messageToSend += args[0] + seperator;
+		}
+		for (let i = 0; i < 4; i++) message.channel.send(messageToSend);
+		message.channel.send(`${args[0]} should probably open Discord and respond to ${message.author.username}.`);
+	}
+};
--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const config = require('./config.json');
+const {
+  prefix,
+  token
+} = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong.');
-}
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('No DM Commands >:(');
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+  }
 });
 
-client.login(config.token);
+client.login(token);
