9720aea0a962792abcfaa40195f8a3d0b483c0a7
HannahGuo <hguo2102@gmail.com>
2020-08-04 22:25:11 -0400

    fixed spam for real this time

--- commands/spam.js
+++ commands/spam.js
@@ -4,11 +4,12 @@ module.exports = {
 	guildOnly: true,
 	usage: '<user>',
 	execute(message, args) {
-		if (args.length != 1 || !args[0].match(/<@.?[0-9]*?>/)) {
+		console.log(message.author);
+		if (args.length != 1 || !args[0].match(/<@.?[0-9]*?>/g)) {
 			message.channel.send("Please only use a tagged user as an argument! Example: !ping @Person");
 			return;
-		} else if (args[0].includes("317326420617592833>") && message.author.username != "Myself⁷") {
-			// Stopping people from spamming my account because... that was happening.
+		} else if (args[0].includes("317326420617592833") && ((message.author.username != "Myself⁷" || message.author.discriminator != "8785") || message.author.bot)) {
+			// Stopping people from spamming my account because... that was happening. My Discord tag is Myself⁷#8785
 			message.channel.send("No you can't spam ping the creator. That's mean :( ");
 			return;
 		}
